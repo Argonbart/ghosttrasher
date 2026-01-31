76b36cfe0325be8cce351f70f80c5d77ed5fda2d
MarkusDennstedt <74238626+MarkusDennstedt@users.noreply.github.com>
2026-01-31 22:35:38 +0100

    Add Talismans and Lock Ghost to Person

--- ghost-trasher/scripts/interaction_manager.gd
+++ ghost-trasher/scripts/interaction_manager.gd
@@ -6,53 +6,80 @@ var player: CharacterBody2D
 
 const base_text = "[E]"
 
-var active_areas = []
+var active_areas_unmarked = []
+var active_areas_marked = []
 var can_interact: bool = true
-var last_interactable: Interactable
 
 
-func register_area(area: Interactable):
-	active_areas.push_back(area)
+func register_area_unmarked(area: Interactable):
+	active_areas_unmarked.push_back(area)
 
 
-func unregister_area(area: Interactable):
-	var index = active_areas.find(area)
+func register_area_marked(area: Interactable):
+	active_areas_marked.push_back(area)
+
+
+func unregister_area_unmarked(area: Interactable):
+	var index = active_areas_unmarked.find(area)
 	if index != -1:
-		active_areas.remove_at(index)
+		active_areas_unmarked.remove_at(index)
+
+
+func unregister_area_marked(area: Interactable):
+	var index = active_areas_marked.find(area)
+	if index != -1:
+		active_areas_marked.remove_at(index)
 
 
 func _process(_delta: float) -> void:
-	if active_areas.size() > 0 && can_interact:
-		active_areas.sort_custom(_sort_by_distance_to_player)
-		if active_areas.size() > 0:
-			for i in range(active_areas.size()):
-				active_areas[i].outline.hide()
-		label.text = base_text
-		last_interactable = active_areas[0]
-		label.global_position = active_areas[0].global_position
-		label.global_position.y -= active_areas[0].label_offset
-		label.global_position.x -= label.size.x / 2
-		label.show()
-		active_areas[0].outline.show()
+	if (active_areas_unmarked.size() > 0 || active_areas_marked.size() > 0) && can_interact:
+		if active_areas_marked.size() > 0:
+			active_areas_marked.sort_custom(_sort_by_distance_to_player)
+			label.text = base_text
+			label.global_position = active_areas_marked[0].global_position
+			label.global_position.y -= active_areas_marked[0].label_offset
+			label.global_position.x -= label.size.x / 2
+			label.show()
+		else:
+			active_areas_unmarked.sort_custom(_sort_by_distance_to_player)
+			for i in range(active_areas_unmarked.size()):
+				active_areas_unmarked[i].outline.hide()
+			label.text = base_text
+			label.global_position = active_areas_unmarked[0].global_position
+			label.global_position.y -= active_areas_unmarked[0].label_offset
+			label.global_position.x -= label.size.x / 2
+			label.show()
+			active_areas_unmarked[0].outline.show()
 	else:
 		label.hide()
 
 
 func _sort_by_distance_to_player(area1, area2):
-	var area1_to_player = player.global_position.distance_to(area1.global_position)
-	var area2_to_player = player.global_position.distance_to(area2.global_position)
-	return area1_to_player < area2_to_player
+		var area1_to_player = player.global_position.distance_to(area1.global_position)
+		var area2_to_player = player.global_position.distance_to(area2.global_position)
+		return area1_to_player < area2_to_player
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") && can_interact:
-		if active_areas.size() > 0:
+		if active_areas_marked.size() > 0:
 			can_interact = false
 			label.hide()
 			
-			await active_areas[0].interact.call()
+			await active_areas_marked[0].interact.call()
 			
 			can_interact = true
+		elif active_areas_unmarked.size() > 0:
+			can_interact = false
+			label.hide()
+			
+			await active_areas_unmarked[0].interact.call()
+			
+			can_interact = true
+	elif event.is_action_pressed("kill") && can_interact:
+		print("töten")
+
+
 
 func _set_player():
 	player = get_tree().get_first_node_in_group("player")
