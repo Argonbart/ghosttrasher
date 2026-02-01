0d72b121bd240d1e7320b7635988445af1d59ece
Alex <104043084+Argonbart@users.noreply.github.com>
2026-02-01 02:25:05 +0100

    Refactor interactable code + add talisman sprite

--- ghost-trasher/scripts/interactable.gd
+++ ghost-trasher/scripts/interactable.gd
@@ -1,32 +1,27 @@
 extends Area2D
 class_name Interactable
 
-@export var is_interactable: bool = true
-@export var outline:AnimatedSprite2D
-@export var parent_object: Node2D
+
+@export var outline: AnimatedSprite2D
 
 
-var interact: Callable = func():
+func interact():
 	get_parent()._on_interact()
-	pass
-
-var kill: Callable = func():
-	pass
-
-func _on_body_entered(_body: Node2D) -> void:
-	
-	# Case Human
-	if parent_object is Human:
-		if WorldManager.current_state == WorldManager.WORLD_STATE.HUMAN_WORLD:
-			if !parent_object.marked_by_talisman:
-				InteractionManager.register_area(self)
-	
-	# Hier checken auf andere Dinge
-	# if parent_object is DjBooth:
 
 
-func _on_body_exited(_body: Node2D) -> void:
-	if parent_object is Human:
-		if !parent_object.marked_by_talisman:
-			outline.hide()
-			InteractionManager.unregister_area(self)
+func kill():
+	if get_parent() is Human:
+		get_parent()._on_kill()
+
+
+func steal():
+	if get_parent() is Human:
+		get_parent()._on_steal()
+
+
+func _on_body_entered(_body) -> void:
+	InteractionManager.register_area(self)
+
+
+func _on_body_exited(_body) -> void:
+	InteractionManager.unregister_area(self)
--- ghost-trasher/scripts/interaction_manager.gd
+++ ghost-trasher/scripts/interaction_manager.gd
@@ -1,57 +1,82 @@
 extends Node2D
 
+
 var areas = []
+
 
 func _ready() -> void:
 	WorldManager.connect("world_state_changed", _on_world_state_changed)
 
 
 func _on_world_state_changed(new_state: WorldManager.WORLD_STATE):
-	var remove_counter = 0
+	
+	# remove humans on swap to ghost world
 	if new_state == WorldManager.WORLD_STATE.GHOST_WORLD:
-		for i in range(areas.size()):
-			if areas[i - remove_counter].get_parent() is Human:
-				areas[i - remove_counter].outline.hide()
-				areas.remove_at(i - remove_counter)
-				remove_counter += 1 
-	else:
-		for i in range(areas.size()):
-			if areas[i - remove_counter].get_parent() is not Human:
-				areas[i - remove_counter].outline.hide()
-				areas.remove_at(i - remove_counter)
-				remove_counter += 1 
+		var idx = 0
+		while idx < areas.size():
+			if areas[idx].get_parent() is Human:
+				areas[idx].outline.hide()
+				areas.remove_at(idx)
+				idx -= 1
+			idx += 1
+	
+	# remove all non-humans on swap to human world
+	if new_state == WorldManager.WORLD_STATE.HUMAN_WORLD:
+		var idx = 0
+		while idx < areas.size():
+			if areas[idx].get_parent() is not Human:
+				areas[idx].outline.hide()
+				areas.remove_at(idx)
+				idx -= 1
+			idx += 1
+
 
 func register_area(area: Interactable):
+	
+	# register only humans in human world
 	if area.get_parent() is Human and WorldManager.current_state == WorldManager.WORLD_STATE.HUMAN_WORLD:
 		areas.append(area)
-		
+	
+	# register only non-humans in ghost world
 	if area.get_parent() is not Human and WorldManager.current_state == WorldManager.WORLD_STATE.GHOST_WORLD:
 		areas.append(area)
 
 
 func unregister_area(area: Interactable):
+	area.outline.hide()
 	areas.erase(area)
 
 
-func _process(_delta: float) -> void:	
-	if areas.size() > 0:
+func _process(_delta: float) -> void:
+	
+	# show outline of closest area in areas
+	if areas.size() > 0 and WorldManager.current_state:
 		areas.sort_custom(_sort_by_distance_to_player)
-		for i in range(areas.size()):
-			areas[i].outline.hide()
+		for area in areas:
+			area.outline.hide()
 		areas[0].outline.show()
+
+
+func _input(event: InputEvent) -> void:
+	
+	# nothing to interact with
+	if not areas.is_empty():
+		return
+	
+	# interact with next interactable
+	if event.is_action_pressed("interact"):
+		areas[0].interact.call()
+	
+	# kill if next to human
+	if event.is_action_pressed("kill") and areas[0] is Human:
+		areas[0].kill.call()
+	
+	# steal if next to human
+	if event.is_action_pressed("steal") and areas[0] is Human:
+		areas[0].steal.call()
 
 
 func _sort_by_distance_to_player(area1, area2):
 		var area1_to_player = Globals.player.global_position.distance_to(area1.global_position)
 		var area2_to_player = Globals.player.global_position.distance_to(area2.global_position)
 		return area1_to_player < area2_to_player
-
-
-func _input(event: InputEvent) -> void:
-	if areas.is_empty():
-		return
-	
-	if event.is_action_pressed("interact"):
-		areas[0].interact.call()
-	elif event.is_action_pressed("kill"):
-		print("töten")
